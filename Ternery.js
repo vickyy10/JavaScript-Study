e31583641ce31d2853dc23293b065a1f1b40d5f3
vignesh <viignesh7@gmail.com>
2024-09-04 11:01:53 +0530

    first commit

---
+++ Ternery.js
@@ -0,0 +1,7 @@
+
+
+k=10
+
+k<0? (console.log('hii')):k=10?(console.log("one zero")):(console.log("helooo"))
+
+
---
+++ Variables.js
@@ -0,0 +1,17 @@
+
+
+let a='vignesh'
+a='vickyy' //we can re reasign
+// let a='vickyyy'  we cant redeclare let 
+
+
+var  b='vignesh'
+b='vickyy ' //can reasign
+var b='vikcyyy' //can re declare
+
+
+const c='vignesh' //cant re declare or reasign
+
+console.log(a);
+console.log(b);
+console.log(c);
